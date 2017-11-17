3e0144afdcdbda6840659f88a739d333b243a53f
Sandra <33315937+sandrahdezledesma@users.noreply.github.com>
2017-11-17 19:41:28 +0100

    Add files via upload

---
+++ PROGRAMA 3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+int main(){
+    /*Este programa sirve para leer tres numeros enteros
+    y devolver el mayor*/
+    int n1,n2,n3,mayor;
+    std::cout<<"Escribe el primer numero: ";
+    std::cin>>n1;
+    std::cout<<"Escribe el segundo numero: ";
+    std::cin>>n2;
+    std::cout<<"Escribe el tercer numero: ";
+    std::cin>>n3;
+    if(n1>n2);{
+             if(n1>n3);
+             mayor=n1;
+             }  
+             };    
+    else{
+         mayor=n3;
+    }   
+    else{
+    if (n2>n3);{
+        mayor=n2;
+    }
+    else{
+         mayor=n3;
+       }  
+    {
+    std::cout<<"El mayor es "<<mayor;
+    std:cout<<"Toca cualquier tecla: ";
+    std::cout>>salir;
+    return 0;
