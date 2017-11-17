d70034e2216bd5133fda515bd34cb162343ec779
Sandra <33315937+sandrahdezledesma@users.noreply.github.com>
2017-11-17 19:37:49 +0100

    Add files via upload

---
+++ PROGRAMA 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+    int salir;
+    
+    std::cout<<"Hola don Pepito";
+    std::cout<<"Toca cualquier tecla para terminar";
+    std::cin>>salir;
+    return 0;
+}  
+    
+    
+    
+    
+    
