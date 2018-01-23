67713513ecb6684bf274122272230a84519ec381
Sandra <33315937+sandrahdezledesma@users.noreply.github.com>
2018-01-23 11:54:28 +0100

    Add files via upload

---
+++ PROGRAMA 18 -Punteros.cpp
@@ -0,0 +1,11 @@
+//PUNTEROS- modo de otra version mas antigua
+#include<stdio.h>
+
+int main(){
+    int a=3;
+    int b=2;
+    int salir;
+    printf("%x",&a); //&: la direccion de memoria de A, numero hexadecimal 
+    scanf("%c",&b); //c de character //scanf es el cin antiguo//&: forma de llamar a un puntero, 
+// el resultado es donde esta localizado
+}
---
+++ PROGRAMA 18- PUNTEROS 2.cpp
@@ -0,0 +1,12 @@
+//PUNTEROS PARTE 3
+#include<stdio.h>
+
+int main(){
+    int a=3;
+    int b=2;
+    int salir;
+    printf("direccion de a= %x\n",&a); //\n es retorno de carro, salta ala linea siguiente
+    printf("direccion de b= %x",&b); 
+    scanf("%c",&salir);
+// el resultado es donde esta localizado
+}
