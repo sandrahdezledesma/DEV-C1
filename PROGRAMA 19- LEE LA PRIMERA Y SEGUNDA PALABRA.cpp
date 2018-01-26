c533a6e7b41740270d5b0a6c347a28f42a35ef6f
Sandra <33315937+sandrahdezledesma@users.noreply.github.com>
2018-01-26 12:18:19 +0100

    Add files via upload

---
+++ PROGRAMA 19- LEE LA PRIMERA Y SEGUNDA PALABRA.cpp
@@ -0,0 +1,17 @@
+//PUNTEROS PARTE 4- lee primera y segunda palabra
+#include<stdio.h>
+
+int main(){
+    char palabra1[5];
+    char palabra2[5];
+    printf("\nDime algo: ");
+    scanf("%s", palabra1);
+    printf("Dime algo mas: ");
+    scanf("%s",palabra2);
+    printf("Direccion palabra1= %x",palanra1;
+    int salir;
+    printf("palabra1 = %s\n",palabra1); //\n es retorno de carro, salta ala linea siguiente
+    printf("palabra2 = %s",palabra2); 
+    scanf("%i",&salir);
+// el resultado es donde esta localizado
+}
